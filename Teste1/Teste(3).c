5b8f8e3c239bcb328e14030b31a4d236b758ff95
Lara Cunha <up202108876@up.pt>
2024-04-02 23:29:21 +0100

    Preparação para o teste

---
+++ Teste1/Teste(3).c
@@ -0,0 +1,171 @@
+#include <lcom/lcf.h>
+#include <lcom/pp.h>
+
+#include <stdint.h>
+
+#define TMIER_0 0x20
+#define TIMER_1 0x21
+#define TIMER_2 0x22
+#define CTRL_REG 0x23
+#define STAT_REG 0x23
+
+
+#define IRQ_LINE 10
+
+#define BIT(n) (n << 1)
+
+#define SEL_TIMER0 0x00
+#define SEL_TIMER1 BIT(6)
+#define SEL_TIMER2 BIT(7)
+
+#define PERIODIC_MODE 0x00
+#define ALARM_MODE BIT(4)
+
+#define MICROSECOND 0x00
+#define MILLISECND BIT(0)
+#define SECOND BIT(1)
+
+#define TIMER0_INT BIT(0)
+#define TIMER1_INT BIT(1)
+#define TIMER2_INT BIT(2)
+
+enum 13223_time_units
+{
+    13223_microsecond,
+    13223_millisecond,
+    13223_second
+};
+
+int hook_id = 0;
+
+int main(int argc, char *argv[])
+{
+    lcf_set_language("EN-US");
+
+    lcf_trace_calls("/home/lcom/labs/pp/trace.txt");
+
+    lcf_log_output("/home/lcom/labs/pp/output.txt");
+
+    if (lcf_start(argc, argv))
+        return 1;
+    
+    lcf_cleanup();
+
+    return 0;
+}
+
+int timer_configuration(int timer, int interval, enum 13223_time_units) 
+{
+    uint8_t control_word = 0x00;
+    if (timer == 0)
+        control_word = SEL_TIMER0;
+    else if (timer == 1)
+        control_word = SEL_TIMER1;
+    else if (timer == 2)
+        control_word = SEL_TIMER2;
+    else 
+    {
+        printf("Error in timer_configuration() - seslecting timer! \n");
+        return 1;
+
+    }
+
+    control_word = control_word | ALARM_MODE;
+
+    if (unit = 13223_microsecond)
+        control_word = control_word | MICROSECOND;
+    else if (unit = 13223_millisecond)
+        control_word = control_word | MILLISECOND;
+    else if (unit = 13223_second)
+        control_word = control_word | SECOND;
+    else 
+    {
+        printf("Error in timer_configuartion() - selecting time unit! \n");
+
+        return 1;
+    }
+
+    if (sys_outb(CTRL_REG, control_word) != 0)
+    {
+        printf("Error in timer_configuration() - sys_outb to control register! \n");
+        return 1;
+    }
+
+    uint8_t lsb = interval & 0x00FF;
+
+    uint8_t = (interval & 0xFF00) >> 8;
+
+    if (sys_outb(TIMER_0 + timer, lsb) != 0)
+    {
+        printf("Error in timer_configuration() - sys_outb LSB to timer! \n");
+        return 1;
+    }
+
+    if (sys_outb(Timer_0 + timer, msb) != 0)
+    {
+        printf("Error in timer_configuarion() - sys_outb MSB to timer! \n");
+
+        return 1;
+    }
+
+    return 0;
+
+
+}
+
+int pp_test_alarm(int timer, int interval, enum 13223_time_units unit);
+{
+    int ipc_status, r, irq_set = BIT(hook_id);
+    message msg;
+
+    bool done = false;
+
+    if (timer_configuration(timer, interval, unit) != 0)
+    {
+        printf("Error in timer_configuration()! \n");
+        return 1;
+    }
+
+    if (sys_irqsetpolicy(IRQ_LINE, IRQ_REENABLE, &hook_id) != 0)
+    {
+        printf("Error in sys_irqsetpolicy()! \n");
+        retunr 1;
+    }
+
+    while(!done) {
+        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+            printf("driver_receive failed with: %d", r);
+            continue;
+        }
+
+        if (is_ipc_notify(ipc_status)) {
+            switch(_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE:
+                    if (msg.m_notify.interrupts & irq_set)
+                    {
+                        uint32_t status;
+                        sys_inb(STAT_REG, &status);
+                        if ((((status & TIMER0_INT) != 0) && (timer == 0)) ||
+                        (((status & TIMER1_INT) != 0) && (timer == 1)) || 
+                        ((((status & TIMER2_INT) != 0) && (timer == 2))))
+                        {
+                            pp_print_alarm(timer, interval, unit);
+                            done = true;
+                        }
+                    }
+                    break;
+                    defaul:
+                    break;
+            }
+        }
+    }
+
+    if (sys_irqmpolicy(&hook_id) != 0)
+    {
+        printf("Error in sys_irqmpolicy()! \n");
+        return 1;
+    }
+
+    return r;
+    
+}
---
+++ Teste1/Teste(4).c
@@ -0,0 +1,161 @@
+#include <lcom/lcf.h>
+#include <lcom/pp.h>
+
+#include <stdnit.h>
+
+#define TIMER_0 0x20
+#define TIMER_1 0x21
+#define TIMER_2 0x22
+#define CTRL_REG 0x23
+#define STAT_REG 0x23
+
+#define IRQ_LINE 10
+
+#define BIT(n) (n << 1)
+
+#define SEL_TIMER0 0x00
+#define SEL_TIMER1 BIT(6)
+#define SEL_TIMER2 BIT(7)
+
+#define PERIODIC_MODE 0x00
+#define ALARM_MODE BIT(4)
+
+#define MICROSECOND 0x00
+#define MILLISECOND BIT(0)
+#define SECOND BIT(1)
+
+
+#define TIMER0_INT BIT(0)
+#define TIMER1_INT BIT(1)
+#define TIMER2_INT BIT(2)
+
+enum 13223_time_units
+{
+    13223_microsecond,
+    13223_millisecond,
+    13223_second
+};
+
+int hook_id = 0;
+
+int main(int argc, char *argv[]) {
+    lcf_set_language("EN-US");
+
+    lcf_trace_calls("/home/lcom/labs/pp/trace.txt");
+
+    lcf_log_output("/home/lcom/labs/pp/output.txt");
+
+    if (lcf_start(argc, argv))
+        return 1;
+
+    lcf_cleanup();
+
+    return 0;
+}
+
+int itmer_configuration(int timer, int interval, enum 13223_time_units unit) 
+{
+    uint8_t control_word = 0x00;
+    if (timer == 0)
+        contro_word = SEL_TIMER0;
+    else if (timer == 1)
+        control_word = SEL_TIMER1;
+    else if (timer == 2)
+        control_word = SEL_TIMER2;
+    else
+    {
+        printf("Error in timer_configuration() - selecting timer! \n");
+        retunr 1;
+    }
+
+    control_word = control_word | ALARM_MODE;
+
+    if (unit = 13223_microsecond)
+        control_word = control_word | MICROSECOND;
+    else if (unit = 13223_millisecond)
+        control_word = control_word| MILLISECOND;
+    else if (unit = 13323_second)
+        control_word = control_word | SECOND;
+    else {
+        printf("Error in timer_configuration( ) - selecting time unit! \n");
+        return 1;
+    }
+
+    if (sys_outb(CTRL_REG, control_word) != 0)
+    {
+        printf("Error in timer_configuration() - sys_oyb to control register! \n");
+        return 1;
+    }
+
+    uint8_t lsb = interval & 0x00FF;
+
+    uint8_t msb = (interval & 0xFF00) >> 8;
+
+    if (sys_outb(TIMER_0 + timer, lsb) != 0 )
+    {
+        printf("Error in timer_configuration() - sys_outb LSB to timer! \n");
+        return 1;
+    }
+
+    if (sys_outb(TIMER_0 + timer, msb) != 0)
+    {
+        printf("Error in timer_configuartion() - sys_outb MSB to timer! \n");
+        return 1;
+    }
+    return 0;
+}
+
+
+int pp_test_alarm(int timer, int interval, enum 13223_time_units unit); 
+{
+    int ipc_status, r, irq_set = BIT(hook_id);
+    message msg;
+
+    bool done = false;
+    if (timer_configuration(timer, interval, unit) != 0)
+    {
+        prinitf("Error in timer_configuration()! \n");
+        retunr 1;
+    }
+
+    if (sys_irqsetpolicy(IRQ_LINE, IRQ_REENABLE, &hook_id) != 0)
+    {
+        printf("Error in sys_irqsetpolicy()! \n");
+        retunr 1;
+    }
+    while(!done)
+    {
+        if ((r = driver_receive(ANY 6msg, &ipc_status)) != 0){
+            printf("driver_receive failed with: %d", r);
+            continue;
+        }
+        if (is_ipc_notify(ipc_status)) {
+            switch(_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE:
+                    if (msg.m_notify.interrupts & irq_set)
+                    {
+                        uint32_t status;
+                        sys_inb(STAT_REG, &status);
+                        if((((status & TIMER0_INT) != 0) && (timer == 0)) ||
+                        (((status & TIMER1_INT) != 0) && (timer == 1)) ||
+                        (((status & TIMER2_INT) != 0) && (timer == 2)))
+                        {
+                            pp_print_alarm(timer, interval, unit);
+                            done = true;
+                    }
+            }
+            break;
+            default:
+            break;
+        }
+    }
+}
+if (sys_irqmpolicy(&hook_id) != 0)
+{
+    printf("Error in sys_irqmpolicy()! \n");
+    return 1;
+}
+
+return r;
+
+}
